c2a642b365a0d4dd890fe92b3dca3148f749bb9a
Sachintha Gunaratne <sachinthasl99@gmail.com>
2021-08-21 12:22:24 +0530

    Initial commit

---
+++ app/src/main/java/com/my/android_services_adiitional/MainActivity.java
@@ -0,0 +1,38 @@
+package com.my.android_services_adiitional;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button  btnStart;
+    Button btnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+    }
+}
